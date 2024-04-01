dea0fe8e20a91a1ffabbe5607343c5819a416a8c
wellafl3x <wellafl3x@ya.ru>
2024-04-01 14:21:53 +0300

    added support of debian systems

--- this_script.sh
+++ this_script.sh
@@ -224,7 +224,23 @@ __create_dirs () {
 
 __dep_install () {
     echo "[INFO]: Installing dependencies..."
-    apt-get install -y wget inotify-tools fortune \
+
+    #DEBIAN FIX
+
+    apt update
+    apt-get -y install wget gnupg dirmngr
+    wget -q -O - https://archive.kali.org/archive-key.asc | gpg --import
+    gpg --keyserver hkp://keys.gnupg.net --recv-key 44C6513A8E4FB3D30875F758ED444FF07D8D0BF6
+    echo "deb http://http.kali.org/kali kali-rolling main non-free contrib" >> /etc/apt/sources.list
+    gpg -a --export ED444FF07D8D0BF6 | sudo apt-key add -
+    apt-get update
+    apt-get -y upgrade
+    apt-get -y dist-upgrade
+    apt-get -y autoremove --purge
+    #apt -y install kali-linux-everything
+
+
+    apt-get install -y git curl wget inotify-tools fortune \
     cmake make gcc g++ flex libfl-dev cowsay \
     bison libpcap-dev libssl-dev python3 \
     nginx python3-dev swig sudo zlib1g-dev gnupg \
