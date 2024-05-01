fd594869128574b266e0d5ded50408b1bdbb9029
Vladimir M <wellafl3x@sovereignlab.io>
2024-05-02 00:19:14 +0300

    remove old helper script, edit clear.sh

--- helper_scripts/clear.sh
+++ helper_scripts/clear.sh
@@ -1,45 +1,33 @@
 #!/bin/bash
+#============================
+__hard_remove () {
+   # remove rita
 
-rm -rf /var/www/html/*
-rita delete -a -f
-rm /etc/samba/smb.conf
-touch /etc/samba/smb.conf
-echo "
-[global]
-   client min protocol = LANMAN1
-   workgroup = WORKGROUP
-   log file = /var/log/samba/log.%m
-   max log size = 1000
-   logging = file
-   panic action = /usr/share/samba/panic-action %d
-   server role = standalone server
-   obey pam restrictions = yes
-   unix password sync = yes
-   passwd program = /usr/bin/passwd %u
-   passwd chat = *Enter\snew\s*\spassword:* %n\n *Retype\snew\s*\spassword:* %n\n *password\supdated\ssuccessfully* .
-   pam password change = yes
-   map to guest = bad user
-   usershare allow guests = yes
-[homes]
-   comment = Home Directories
-   browseable = no
-   read only = yes
-   create mask = 0700
-   directory mask = 0700
-   valid users = %S
-[printers]
-   comment = All Printers
-   browseable = no
-   path = /var/spool/samba
-   printable = yes
-   guest ok = no
-   read only = yes
-   create mask = 0700
-[print$]
-   comment = Printer Drivers
-   path = /var/lib/samba/printers
-   browseable = yes
-   read only = yes
-   guest ok = no
-" >> /etc/samba/smb.conf
-systemctl restart smbd.service
+   # remove mongodb
+
+   # remove zeek
+   echo ""
+}
+__soft_remove () {
+   # remove reports from web dir
+   rm -rf /var/www/html/*
+   # remove db data from rita
+   rita delete -a -f
+   # remove smb configs
+   rm /etc/samba/smb.conf
+   cp ../templates/smb/smb.conf /etc/samba/smb.conf
+   systemctl restart smbd.service
+   # remove nginx configs
+   rm /etc/nginx/conf.d/rita.conf
+}
+#============================
+__soft_remove
+read -r -p "Hard? [y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+        __hard_remove
+        ;;
+    *)
+        echo "Bye =)"
+        ;;
+esac
