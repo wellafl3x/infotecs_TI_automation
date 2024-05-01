ff40b4aeb1c00bfd72ceafbea78edf9817421cb9
wellafl3x <wellafl3x@ya.ru>
2024-05-01 23:06:22 +0300

    added comments for all func, add templates for config files, remove sleepers

--- this_script.sh
+++ this_script.sh
@@ -1,33 +1,22 @@
 #!/bin/bash
 # Automation solution for analyzing PCAP files with Zeek and RITA frameworks.
 # Written by wellafl3x.
-
-#+++++++OS_DETECTION+++++++
-# Support of other distros would be implement here
-DISTR=null
-if type lsb_release >/dev/null 2>&1; then
-    distro=$(lsb_release -i -s)
-    if [ "$distro" == "Debian" ]; then
-      DISTR="Debian"
-    elif [ "$distro" == "Ubuntu" ]; then
-      DISTR="Ubuntu"
-    fi
-else
-    echo "Couldn't tell, which distro this system uses. Aborting..."
-    exit 1
-fi
+# TODO:
+# 1. Check system compability, add CentOS support
+# 2. Add support of various OS in __dep_install func
+# 3. Add support of various OS in __zeek_install func
+# 4. Add support of various OS in __mongo_install func
+# 5. Test and debug in diff distro
 
 #========CONST_VARS========
-if [[ ! -z ${PATH_TO} ]] && [ -d "$PATH_TO" ]; then # check if other path defined
-    ROOTDIR="${PATH_TO}"
-else
-    ROOTDIR=$HOME
-fi
+#dirs and files
 SOURCE_DIR="$PWD"
 PCAP_DIR="$ROOTDIR"/PCAPS
 ZEEK_DIR=/tmp/ZEEK
 RITA_DIR="$ROOTDIR"/REPORTS
 NGINX_DIR=/var/www/html
+RITA_CONF_FILE=/etc/rita/config.yaml
+#flags
 INSTALL_ZEEK=true
 INSTALL_MONGO=true
 INSTALL_RITA=true
@@ -36,6 +25,7 @@ SMB_CONF=false
 #==========================
 
 #========FUNCTIONS========
+# __help will show help message with -h flag
 __help () {
     echo ""
     echo "Usage:"
@@ -64,7 +54,7 @@ __help () {
     echo "--disable-all     Run script without any install processes"
     echo ""
 }
-
+# __check_for_root will check if file running with root priveleges
 __check_for_root () {
 
     if [ "$EUID" -ne 0 ]
@@ -73,8 +63,8 @@ __check_for_root () {
     fi
 
 }
-
-__zastavka () {
+# _hello will display graphic message with team name
+__hello () {
 
     echo "
               ____  ____  _    ____  _   _ _____   ____  __  __ _ _____ __  __  ___  
@@ -85,7 +75,22 @@ __zastavka () {
 
     "
 }
-
+# __os_detection will detect which repo is system using
+__os_detection () {
+    DISTR=null
+    if type lsb_release >/dev/null 2>&1; then
+      distro=$(lsb_release -i -s)
+      if [ "$distro" == "Debian" ]; then
+        DISTR="Debian"
+      elif [ "$distro" == "Ubuntu" ]; then
+        DISTR="Ubuntu"
+      fi
+    else
+      echo "Couldn't tell, which distro this system uses. Aborting..."
+      exit 1
+    fi
+}
+# __create_dirs will create dirs for pcap files analyzing, zeek and rita dirs, web files
 __create_dirs () { 
     if [ ! -d "$PCAP_DIR" ]; then
         mkdir "$PCAP_DIR"
@@ -103,156 +108,24 @@ __create_dirs () {
         rm $NGINX_DIR/index.html
     fi
     touch $NGINX_DIR/index.html
-    echo '
-    <head>
-    <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
-    <meta content="utf-8" http-equiv="encoding">
-    <link rel="stylesheet" type="text/css" href="./style.css">
-    </head>    
-    <p>
-      <div class="info">To view individual databases, click on any of the links below.</div>
-      <div class="vertical-menu">
-        
-
-        
-      </div>
-    </p>
-    
-    ' >> $NGINX_DIR/index.html
-
+    cat ./templates/web/index.html >> $NGINX_DIR/index.html
     if [ -f $NGINX_DIR/style.css ]; then
         rm $NGINX_DIR/style.css
     fi
     touch $NGINX_DIR/style.css
-    echo "
-    p {
-    margin-bottom: 1.625em;
-    font-family: 'Lucida Sans', Arial, sans-serif;
-  }
-  
-  p {
-    font-family: 'Lucida Sans', Arial, sans-serif;
-    text-indent: 30px;
-  }
-  
-  h1 {
-    color: #000;
-    font-family: 'Lato', sans-serif;
-    font-size: 32px;
-    font-weight: 300;
-    line-height: 58px;
-    margin: 0 0 58px;
-    text-indent: 30px;
-  }
-  
-  ul {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-    background-color: #000;
-    font-family: "Arial", Helvetica, sans-serif;
-  }
-  
-  li {
-    float: left;
-    border-right: 1px solid #bbb;
-  }
-  
-  li:last-child {
-    border-right: none;
-  }
-  
-  li a {
-    display: block;
-    color: white;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-  }
-  
-  div {
-    color: #adb7bd;
-    font-family: 'Lucida Sans', Arial, sans-serif;
-    font-size: 16px;
-    line-height: 26px;
-    margin: 0;
-  }
-  
-  li a:hover {
-    background-color: #34C6CD;
-  }
-  
-  .vertical-menu {
-    width: auto;
-  }
-  
-  .vertical-menu a {
-    background-color: #000;
-    color: white;
-    display: block;
-    padding: 12px;
-    text-decoration: none;
-    text-align: center;
-    vertical-align: middle;
-  }
-  
-  .vertical-menu a:hover {
-    background-color: #34C6CD;
-  }
-  
-  .active {
-    background-color: #A66F00;
-    color: white;
-  }
-  
-  .info {
-    margin: 10px 0px;
-    padding:12px;
-    color: white;
-    background-color: #333;
-  }
-  
-  .container {
-    overflow-x: auto;
-    white-space: nowrap;
-  }
-  
-  table {
-    border-collapse: collapse;
-    width: 100%;
-  }
-  
-  th, td {
-    text-align: left;
-    padding: 8px;
-  }
-  
-  tr:nth-child(even){
-    background-color: #f2f2f2
-  }
-  
-  #github {
-    height: 1em;
-  }
-  
-  
-    " >> $NGINX_DIR/style.css
-
-
+    cat ./templates/web/style.css >> $NGINX_DIR/style.css
 }
-
+# __dep_install will install dependencies
 __dep_install () {
     echo "[INFO]: Installing dependencies..."
     apt-get install -y git curl wget inotify-tools fortune \
     cmake make rsync gcc g++ flex libfl-dev cowsay \
     bison libpcap-dev libssl-dev python3 \
     nginx python3-dev swig sudo zlib1g-dev gnupg pip \
-
+    >> /dev/null
     echo "[INFO]: Done."
-    sleep 1
 }
-
+# __smb_configure will install samba, create samba user and attach pcap_dir to samba
 __smb_configure () {
     apt install -y samba >> /dev/null
     echo "Enter username (it must be exists in OS!): "
@@ -274,19 +147,20 @@ __smb_configure () {
     (echo "$smb_password"; sleep 1; echo "$smb_password" ) | sudo smbpasswd -s -a "$smb_username"
     smb_config_path=/etc/samba/smb.conf
     rm $smb_config_path
-    cp ./templates/smb.conf /etc/samba
+    cp ./templates/smb/smb.conf /etc/samba
+    echo "
+    "
     echo "[pcaps]" >> $smb_config_path
     echo "   path = $PCAP_DIR" >> $smb_config_path
     echo "   read only = no" >> $smb_config_path
     echo "   guest ok = no" >> $smb_config_path
     echo "   valid user = $smb_username" >> $smb_config_path
-    systemctl enable smdb.service
-    systemctl start smdb.service
+    systemctl enable smbd.service
+    systemctl start smbd.service
 }
-
+# __zeek install will install zeek to the system
 __zeek_install () {
     echo "[INFO]: Installing ZEEK..."
-    sleep 1 
     if [ "$DISTR" == "Debian" ]; then
         # zeek installation for Debian
         echo 'deb http://download.opensuse.org/repositories/security:/zeek/Debian_12/ /' | sudo tee /etc/apt/sources.list.d/security:zeek.list
@@ -294,6 +168,7 @@ __zeek_install () {
         apt update
         apt install -y zeek-6.0
     elif  [ "$DISTR" == "Ubuntu" ]; then
+        # zeek installation for Ubuntu
         echo 'deb http://download.opensuse.org/repositories/security:/zeek/xUbuntu_22.04/ /' | sudo tee /etc/apt/sources.list.d/security:zeek.list
         curl -fsSL https://download.opensuse.org/repositories/security:zeek/xUbuntu_22.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/security_zeek.gpg > /dev/null
         apt update
@@ -302,13 +177,10 @@ __zeek_install () {
     export PATH=$PATH:/opt/zeek/bin
     echo "export PATH=$PATH:/opt/zeek/bin" >> $HOME/.bashrc
     echo "[INFO]: Done."
-
 }
-
+# __mongo_install will install mongoDB 4.2 to the system
 __mongo_install () { 
-    #mongoDB
     echo "[INFO]: Installing MongoDB..."
-    sleep 1 
     wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_amd64.deb
     sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_amd64.deb
     curl -fsSL https://pgp.mongodb.com/server-4.2.asc | \
@@ -320,18 +192,12 @@ __mongo_install () {
     systemctl daemon-reload
     systemctl start mongod
     systemctl enable mongod
-    sleep 2
-    netstat -luntp
-    sleep 3
     echo "[INFO]: Done."
-    sleep 3
 }
-
+# __rita_install will install Golang 1.20 and RITA from src
 __rita_install () {
     echo "[INFO]: Installing RITA..."
-    sleep 1 
-    #golang
-
+    #installing golang from src
     apt-get update
     apt-get upgrade -y
     wget https://go.dev/dl/go1.20.2.linux-amd64.tar.gz
@@ -339,20 +205,19 @@ __rita_install () {
     export GOROOT=/usr/local/go
     export GOPATH=$HOME/go
     export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
-    #rita
-
-    cd /opt
-    git clone https://github.com/activecm/rita.git
+    #installing rita from src
+    git clone https://github.com/activecm/rita.git /opt
+    qq=$PWD
     cd rita
     make install
+    cd $qq
     sudo mkdir /etc/rita && sudo chmod 755 /etc/rita
     sudo mkdir -p /var/lib/rita/logs && sudo chmod -R 755 /var/lib/rita
     sudo chmod 777 /var/lib/rita/logs
     sudo cp etc/rita.yaml /etc/rita/config.yaml && sudo chmod 666 /etc/rita/config.yaml
     echo "[INFO]: Done."
-    sleep 3
 }
-
+# __zeek_analyze will analyze pcap files and generate zeek tmp logs
 __zeek_analyze () {
     for file in "$PCAP_DIR"/*; do
         if [[ $file == *.pcap ]]; then
@@ -364,7 +229,7 @@ __zeek_analyze () {
         fi
     done
 }
-
+# __rita_analyze will create reports based on zeek logs, and make this reports accessible via 80 port
 __rita_analyze () {
     cd $RITA_DIR
     for dir in $ZEEK_DIR/*/; do ##
@@ -385,60 +250,24 @@ __rita_analyze () {
         fi
     done
 }
-
+# __nginx_conf will configure NGINX web-server to access thru him to rita reports
 __nginx_conf () {
     systemctl start nginx
     rm /etc/nginx/nginx.conf
     touch /etc/nginx/nginx.conf
-    echo "
-        user www-data;
-    worker_processes auto;
-    pid /run/nginx.pid;
-    include /etc/nginx/modules-enabled/*.conf;
-    events {
-            worker_connections 768;
-    }
-    http {
-            sendfile on;
-            tcp_nopush on;
-            types_hash_max_size 2048;
-            server_tokens off;
-            include /etc/nginx/mime.types;
-            default_type application/octet-stream;
-            ssl_protocols TLSv1 TLSv1.1 TLSv1.2 TLSv1.3;
-            ssl_prefer_server_ciphers on;
-            access_log /var/log/nginx/access.log;
-            error_log /var/log/nginx/error.log;
-            gzip on;
-            gzip_vary on;
-            gzip_proxied any;
-            gzip_comp_level 6;
-            gzip_buffers 16 8k;
-            gzip_http_version 1.1;
-            gzip_types text/plain text/css application/json application/javascript text/xml application/xml application/xml+rss text/javascript;
-            include /etc/nginx/conf.d/*.conf;
-            #include /etc/nginx/sites-enabled/*;
-    }
-    " >> /etc/nginx/nginx.conf
+    cat ./templates/web/nginx.conf >> /etc/nginx/nginx.conf
     if [ -f /etc/nginx/conf.d/rita.conf ]; then
-        rm /etc/nginx/conf.d/rita.conf
+      rm /etc/nginx/conf.d/rita.conf
     fi
     touch /etc/nginx/conf.d/rita.conf
-    # shellcheck disable=SC2016
-    echo '
-        server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-        root /var/www/html;
-        server_name _;
-        location / {
-                try_files $uri $uri/ =404;
-        }
-    }
-    ' >> /etc/nginx/conf.d/rita.conf
-    nginx -s reload
+    cat ./templates/web/rita.conf >> /etc/nginx/conf.d/rita.conf
+    if ! nginx -s reload; then
+      echo "[INFO]: Error while reload NGINX."
+    else
+      echo "[INFO]: Nginx reloaded"
+    fi
 }
-
+# __whitelist_generate will generate whitelists of IPs and Domains via main.py file
 __whitelist_generate () {
     cd $SOURCE_DIR
     pip install -r ./requirements.txt --break-system-packages
@@ -448,115 +277,23 @@ __whitelist_generate () {
         exit 1
     fi
 }
-
-
+# __whitelist_attach will attach generated whitelists to rita config file
 __whitelist_attach () {
-    rm /etc/rita/config.yaml
-    echo "
-MongoDB:
-  ConnectionString: mongodb://localhost:27017
-  AuthenticationMechanism: null
-  SocketTimeout: 2
-  TLS:
-    Enable: false
-    VerifyCertificate: false
-    CAFile: null
-  MetaDB: MetaDatabase
-Rolling:
-  DefaultChunks: 24
-LogConfig:
-  LogLevel: 2
-  RitaLogPath: /var/lib/rita/logs
-  LogToFile: true
-  LogToDB: true
-UserConfig:
-  UpdateCheckFrequency: 14
-Filtering:
-  AlwaysInclude: []
-  NeverInclude:
-    - 0.0.0.0/32
-    - 127.0.0.0/8
-    - 169.254.0.0/16
-    - 224.0.0.0/4
-    - 255.255.255.255/32
-    - ::1/128
-    - fe80::/10
-    - ff00::/8" >> /etc/rita/config.yaml
-    sleep 2
-    cat $WHITELIST_FILE >> /etc/rita/config.yaml
-    sleep 2 
-    echo "
-  InternalSubnets:
-    - 10.0.0.0/8
-    - 172.16.0.0/12
-    - 192.168.0.0/16
-  AlwaysIncludeDomain: []
-  NeverIncludeDomain:" >> /etc/rita/config.yaml
-    sleep 2
+    rm $RITA_CONF_FILE
+    cp ./templates/rita/config.yaml $RITA_CONF_FILE
+    ip_match="  # IP WHITELIST goes here"
+    sed -i "/$ip_match/r $WHITELIST_FILE" $RITA_CONF_FILE
+    dom_match='  # DOMAINS WHITELIST goes here'
+    dom_replace="  NeverIncludeDomain: []"
     if [ "$DOM_FLAG" = "true" ]; then
-      cat $DOMAINS_FILE >> /etc/rita/config.yaml
+      sed -i "/$dom_match/r $DOMAINS_FILE" $RITA_CONF_FILE
     else
-      cat " []"
+      sed -i "s/$dom_match/$dom_match\n$dom_replace/" $RITA_CONF_FILE
     fi    
-    sleep 2
-    echo "
-  FilterExternalToInternal: true
-BlackListed:
-  Enabled: true
-  feodotracker.abuse.ch: true
-  BlacklistDatabase: "rita-bl"
-  CustomIPBlacklBlacklists: []
-Beacon:
-  Enabled: true
-  DefaultConnectionThresh: 23
-  TimestampScoreWeight: 0.25
-  DatasizeScoreWeight: 0.25
-  DurationScoreWeight: 0.25
-  HistogramScoreWeight: 0.25
-  DurationMinHoursSeen: 6
-  DurationConsistencyIdealHoursSeen: 12
-  HistogramBimodalBucketSize: 0.05
-  HistogramBimodalOutlierRemoval: 1
-  HistogramBimodalMinHoursSeen: 11
-
-BeaconSNI:
-  Enabled: true
-  DefaultConnectionThresh: 23
-  TimestampScoreWeight: 0.25
-  DatasizeScoreWeight: 0.25
-  DurationScoreWeight: 0.25
-  HistogramScoreWeight: 0.25
-  DurationMinHoursSeen: 6
-  DurationConsistencyIdealHoursSeen: 12
-  HistogramBimodalBucketSize: 0.05
-  HistogramBimodalOutlierRemoval: 1
-  HistogramBimodalMinHoursSeen: 11
-
-BeaconProxy:
-  Enabled: true
-  DefaultConnectionThresh: 23
-  TimestampScoreWeight: 0.333
-  DurationScoreWeight: 0.333
-  HistogramScoreWeight: 0.333
-  DurationMinHoursSeen: 6
-  DurationConsistencyIdealHoursSeen: 12
-  HistogramBimodalBucketSize: 0.05
-  HistogramBimodalOutlierRemoval: 1
-  HistogramBimodalMinHoursSeen: 11
-      
-DNS:
-  Enabled: true
-
-UserAgent:
-  Enabled: true
-
-Strobe:
-  ConnectionLimit: 86400" >> /etc/rita/config.yaml
 }
 #=========================
 
 #==========MAIN BODY==========
-
 # Parse command args
 while [[ $# -gt 0 ]]; do
         case $1 in
@@ -568,7 +305,8 @@ while [[ $# -gt 0 ]]; do
                 if [ -z ${WHITELIST+x} ] && [ -z ${DOMAINS+x} ]; then
                     WHITELIST_GEN_FLAG=true
                 else
-                    echo "You cannot generate whitelist file because you set WHITELIST and DOMAINS vars"
+                    echo "You cannot generate whitelist file because you set WHITELIST and DOMAINS vars."
+                    echo "Please, see help page (-h) or README file."
                     exit 1
                 fi
                 ;;
@@ -597,7 +335,12 @@ while [[ $# -gt 0 ]]; do
         shift
     done
 __check_for_root
-__zastavka
+__hello
+if [[ ! -z ${PATH_TO} ]] && [ -d "$PATH_TO" ]; then # check if other path defined
+    ROOTDIR="${PATH_TO}"
+else
+    ROOTDIR=$HOME
+fi
 if [[ ! -z ${WHITELIST} ]] && [ -f "$WHITELIST" ]; then # check if other path defined
     WHITELIST_FILE="${WHITELIST}"
     CHANGE_CONFIG=true
@@ -641,11 +384,10 @@ if [ "$WHITELIST_GEN_FLAG" = "true" ]; then
     WHITELIST_FILE="$PWD/results.txt"
     DOMAINS_FILE="$PWD/domains.txt"
 fi
-sleep 3
 if [ "$CHANGE_CONFIG" = "true" ]; then
     __whitelist_attach
 fi
-/usr/games/cowsay -f eyes "The system has been started. Can be accessible on 80 port. Press Q to exit."
+/usr/games/cowsay -f eyes "The system has been started. Can be accessible on 80 port."
 inotifywait \
   "$PCAP_DIR" \
   --monitor \
